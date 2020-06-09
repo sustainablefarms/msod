66952246e6a6104558701575c9b15b3aa9803be6
Kassel Hingee <kassel.hingee@gmail.com>
2020-06-09 22:58:46 +1000

    more testing of calc predictions, haven't found any more bugs

--- tests/testthat/test-predcalculations.R
+++ tests/testthat/test-predcalculations.R
@@ -86,7 +86,7 @@ test_that("pdetect_indvisit is correct with LV", {
 test_that("pdetect_condoccupied and poccupy_species keeps ordering of sites / visits", {
   ObsFmla = "~ UpVisit - 1"
   OccFmla = "~ UpSite - 1"
-  u.b.min <- matrix((1:5)/8 , ncol = 1)
+  u.b.min <- matrix((1:5)/3 , ncol = 1)
   u.b.max <- u.b.min + 1E-5
   v.b.min <- matrix((1:5)/8 , ncol = 1)
   v.b.max <- v.b.min + 1E-5
@@ -112,4 +112,61 @@ test_that("pdetect_condoccupied and poccupy_species keeps ordering of sites / vi
   expect_true(min(pDetCondOcc[-1, ] - pDetCondOcc[-nrow(pDetCondOcc), ]) > 0)
   expect_true(min(sign(fit$data$Xobs[, "UpVisit"]) * (pDetCondOcc[, -1] - pDetCondOcc[, -ncol(pDetCondOcc)])) > 0)
   
+  # I expect pdetect_indvisit to have the same rows as Xobs
+  pdetect <- pdetect_indvisit(fit, type = 1, conditionalLV = FALSE)
+  # probability of occupancy and detection increases with Site and Visit, 
+  # so full probability of detection increases except possibly whenever ModelSite returns back to 1
+  rowdiff <- pdetect[-1, ] - pdetect[-nrow(pDetCondOcc), ]
+  expect_true(min( rowdiff[fit$data$ModelSite[-1] != 1, ] ) > 0)
+  expect_true(max(rowdiff[fit$data$ModelSite[-1] == 1, ]) < 0) # this is because the Site occupancy effect is stronger
+  
+  # across the rows (species) when UpSite < 0 and UpVisit < 0, I expect the full probability of detection to decrease
+  coldiff <-  pdetect[, -1] - pdetect[, -ncol(pdetect)]
+  expect_lt(max(coldiff[fit$data$Xocc[fit$data$ModelSite, "UpSite"] < 0 & fit$data$Xobs[, "UpVisit"] < 0, ]), 0)
+  # across the rows (species) when UpSite > 0 and UpVisit > 0, I expect the full probability of detection to increase
+  expect_gt(min(coldiff[fit$data$Xocc[fit$data$ModelSite, "UpSite"] > 0 & fit$data$Xobs[, "UpVisit"] > 0, ]), 0)
+  # in other rows I expect there to be a strong pattern, it is probably predictable
 })
+
+test_that("pdetect_condoccupied and poccupy_species ordering of sites / visits with LV", {
+  ObsFmla = "~ 1"
+  OccFmla = "~ 1"
+  u.b.min <- matrix((1:5)/3 , ncol = 1)
+  u.b.max <- u.b.min + 1E-5
+  v.b.min <- matrix((1:5)/8 , ncol = 1)
+  v.b.max <- v.b.min + 1E-5
+  lv.coef.min <- matrix((1:5)/8, ncol = 2, nrow = 5, byrow = FALSE)
+  lv.coef.max <- lv.coef.min + 1E-5
+  
+  fit <- artificial_runjags(nspecies = 5, nsites = 100, nvisitspersite = 6, nlv = 2,
+                            u.b.min = u.b.min,
+                            u.b.max = u.b.max,
+                            v.b.min = v.b.min,
+                            v.b.max = v.b.max,
+                            lv.coef.min = lv.coef.min,
+                            lv.coef.max = lv.coef.max,
+                            OccFmla = OccFmla,
+                            ObsFmla = ObsFmla)
+  LV <- bugsvar2matrix(get_theta(fit, type = 1), "LV", 1:100, 1:2)
+  # should be a matrix that increase down the rows 
+  # when scaled UpSite positive it should go up across the columns
+  # when scaled UpSite negative it should go down the columns
+  pOccupancy <- poccupy_species(fit, type = 1, conditionalLV = TRUE)
+  expect_equivalent(apply(pOccupancy[LV[, 1] > 0 & LV[, 2] > 0, ], 2, sd), rep(0, ncol(pOccupancy)))
+  expect_equivalent(apply(pOccupancy[LV[, 1] > 0 & LV[, 2] < 0, ], 2, sd), rep(0, ncol(pOccupancy)))
+  expect_equivalent(apply(pOccupancy[LV[, 1] < 0 & LV[, 2] < 0, ], 2, sd), rep(0, ncol(pOccupancy)))
+  expect_equivalent(apply(pOccupancy[LV[, 1] < 0 & LV[, 2] > 0, ], 2, sd), rep(0, ncol(pOccupancy)))
+  expect_gt(min(apply(pOccupancy[LV[, 2] > 0, ], 2, sd)), 0)
+  
+  # expect occupancy to go up with species
+  expect_true(min(pOccupancy[, -1] - pOccupancy[, -ncol(pOccupancy)]) > 0)
+  
+  # I expect pdetect_indvisit to have the same rows as Xobs
+  pdetect <- pdetect_indvisit(fit, type = 1, conditionalLV = FALSE)
+  # Similar pattern of equal probabilities as pOccupancy
+  expect_equivalent(apply(pdetect[LV[fit$data$ModelSite, 1] > 0 & LV[fit$data$ModelSite, 2] > 0, ], 2, sd), rep(0, ncol(pdetect)))
+  expect_equivalent(apply(pdetect[LV[fit$data$ModelSite, 1] > 0 & LV[fit$data$ModelSite, 2] < 0, ], 2, sd), rep(0, ncol(pdetect)))
+  expect_equivalent(apply(pdetect[LV[fit$data$ModelSite, 1] < 0 & LV[fit$data$ModelSite, 2] < 0, ], 2, sd), rep(0, ncol(pdetect)))
+  expect_equivalent(apply(pdetect[LV[fit$data$ModelSite, 1] < 0 & LV[fit$data$ModelSite, 2] > 0, ], 2, sd), rep(0, ncol(pdetect)))
+})
+
