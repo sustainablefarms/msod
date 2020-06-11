8b24f18df49892b714483bf6fdef7b16a3d0d599
Kassel Hingee <kassel.hingee@gmail.com>
2020-06-11 23:13:45 +1000

    exporting as.list.format

--- R/calcpredictions.R
+++ R/calcpredictions.R
@@ -184,6 +184,7 @@ get_theta <- function(fit, type){
 #' @title A quick replacement to [runjags::list.format()] that does nothing if the argument is already a list.
 #' @param data Same as [runjags::list.format()]. Typically found in the `data` slot of runjags object.
 #' @param checkvalid See [runjags::list.format()].
+#' @export
 as.list.format <- function(data, checkvalid = TRUE){
   if ("list" %in% class(data)){return(data)}
   out <- runjags::list.format(data, checkvalid = checkvalid)
