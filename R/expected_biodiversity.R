e168e60fadacd55cd7648f425c58bfb4971f9ad9
Kassel Hingee <kassel.hingee@gmail.com>
2020-07-08 17:35:44 +1000

    expected biodiversity interface improved

--- R/expected_biodiversity.R
+++ R/expected_biodiversity.R
@@ -7,36 +7,90 @@
 #' Xocc <- fit$data$Xocc[2, , drop = FALSE]
 #' Xobs <- fit$data$Xobs[fit$data$ModelSite == 2, , drop = FALSE]
 #' numspecies <- fit$data$n
+#' lvsim <- matrix(rnorm(2 * 1), ncol = 2, nrow = 2) #dummy lvsim vars
+#' lv.coef.bugs <- matrix2bugsvar(matrix(0, nrow = fit$data$n, ncol = 2), "lv.coef")
+#' theta <- c(theta, lv.coef.bugs)
 #' 
 #' @param Xocc A matrix of occupancy covariates. Must have a single row. Columns correspond to covariates.
-#' @param Xobs A matrix of detection covariates, each row is a visit.
+#' @param Xobs A matrix of detection covariates, each row is a visit. If NULL then expected number of species in occupation is returned
 #' @param theta A vector of model parameters, labelled according to the BUGS labelling convention seen in runjags
-#' @param lvsim A matrix of simulated LV values. Columns correspond to latent variables, each row is a simulation
 #' @export
-expectedspeciesnum.ModelSite.theta <- function(Xocc, Xobs, numspecies, theta){
-  u.b <- bugsvar2array(theta, "u.b", 1:numspecies, 1:ncol(Xocc))[,,1]  # rows are species, columns are occupancy covariates
-  v.b <- bugsvar2array(theta, "v.b", 1:numspecies, 1:ncol(Xobs))[,,1]  # rows are species, columns are observation covariates
+expectedspeciesnum.ModelSite.theta <- function(Xocc, Xobs = NULL, numspecies, theta, LVvals){
+  stopifnot(nrow(Xocc) == 1)
   Xocc <- as.matrix(Xocc)
-  Xobs <- as.matrix(Xobs)
   
-  ## Probability of Site Occupancy without LV
+  u.b <- bugsvar2array(theta, "u.b", 1:numspecies, 1:ncol(Xocc))[,,1]  # rows are species, columns are occupancy covariates
+  lv.coef <- bugsvar2array(theta, "lv.coef", 1:numspecies, 1:ncol(LVvals))[,,1] # rows are species, columns are lv
+  sd_u_condlv <- sqrt(1 - rowSums(lv.coef^2)) #for each species the standard deviation of the indicator random variable 'u', conditional on values of LV
+
+  
+  ## Probability of Site Occupancy
+  # external
   ModelSite.Occ.eta_external <- as.matrix(Xocc) %*% t(u.b) #columns are species
-  ModelSite.Occ.Pred <- 1 - pnorm(-ModelSite.Occ.eta_external, mean = 0, sd = 1)
   
-  ## Expected number of species occupying modelsite, given model and theta
-  Enumspecies_occ <- Rfast::rowsums(ModelSite.Occ.Pred)
+  # probability of occupancy given LV
+  ModelSite.Occ.eta_LV <- LVvals %*% t(lv.coef) #occupancy contribution from latent variables, performed all together
+  ModelSite.Occ.eta <- Rfast::eachrow(ModelSite.Occ.eta_LV, ModelSite.Occ.eta_external, oper = "+") #add the external part to each simulation
+  # Make u standard deviations equal to 1 by dividing other values by sd
+  # P(u < -ModelSite.Occ.eta) = P(u / sd < -ModelSite.Occ.eta / sd) = P(z < -ModelSite.Occ.eta / sd)
+  ModelSite.Occ.eta_standardised <- Rfast::eachrow(ModelSite.Occ.eta, sd_u_condlv, oper = "/") 
+  ModelSite.Occ.Pred.CondLV <- 1 - pnorm(-ModelSite.Occ.eta_standardised, mean = 0, sd = 1)
   
+  ## Expected number of species occupying modelsite, given model and theta, and marginal across LV
+  Enumspecies_occ <- mean(Rfast::rowsums(ModelSite.Occ.Pred.CondLV))
+  if (is.null(Xobs)){
+    return(Enumspeciesocc = Enumspecies_occ)
+  }
   
   ## Probability of Detection, CONDITIONAL on occupied
-  Detection.LinPred <- as.matrix(Xobs) %*% t(v.b)
-  Detection.Pred.Cond <- exp(Detection.LinPred) / (exp(Detection.LinPred) + 1)   #this is the inverse logit function
-  # probability of no detections, given occupied
-  NoDetections.Pred.Cond <- Rfast::colprods(1 - Detection.Pred.Cond)
+  if (!is.null(Xobs)){
+    Xobs <- as.matrix(Xobs)
+    v.b <- bugsvar2array(theta, "v.b", 1:numspecies, 1:ncol(Xobs))[,,1]  # rows are species, columns are observation covariates
+    Detection.LinPred <- as.matrix(Xobs) %*% t(v.b)
+    Detection.Pred.Cond <- exp(Detection.LinPred) / (exp(Detection.LinPred) + 1)   #this is the inverse logit function
+    # probability of no detections, given occupied
+    NoDetections.Pred.Cond <- Rfast::colprods(1 - Detection.Pred.Cond)
   
-  ## probability of no detections, marginal on occupancy
-  NoDetections.Pred.marg_1 <- Rfast::eachrow(ModelSite.Occ.Pred, NoDetections.Pred.Cond)  #occupied component
-  NoDetections.Pred.marg_2 <- 1 - ModelSite.Occ.Pred  #plus unoccupied component
-  NoDetections.Pred.marg <- NoDetections.Pred.marg_1 + NoDetections.Pred.marg_2
-  Enumspecies_detected <- numspecies - sum(NoDetections.Pred.marg)
-  return(Enumspecies_detected)
+    ## probability of no detections, marginal on occupancy
+    NoDetections.Pred.marg_1 <- Rfast::eachrow(ModelSite.Occ.Pred.CondLV, NoDetections.Pred.Cond, oper = "*")  #occupied component
+    NoDetections.Pred.marg_2 <- 1 - ModelSite.Occ.Pred.CondLV  #plus unoccupied component
+    NoDetections.Pred.marg <- NoDetections.Pred.marg_1 + NoDetections.Pred.marg_2
+    Enumspecies_detected <- numspecies - Rfast::rowsums(NoDetections.Pred.marg)
+    Enumspecies_detected_margLV <- mean(Enumspecies_detected)
+    return(Enumdetected = Enumspecies_detected_margLV)
+  }
+}
+
+#' @describeIn expectedspeciesnum For ModelSite information, predicts both the expected number of species in occupation, and the expected number of species detected.
+#' @param LVvals A matrix of LV values. Each column corresponds to a LV. To condition on specific LV values, provide a matrix of row 1.
+#' If LVvals isn't provided then expections are marginalised across possible LVvalues through simulation
+expectedspeciesnum.ModelSite <- function(fit, Xocc, Xobs, chains = NULL, LVvals = NULL){
+  stopifnot(nrow(Xocc) == 1)
+  Xocc <- as.matrix(Xocc)
+  Xobs <- as.matrix(Xobs)
+  if (is.null(chains)){chains <- 1:length(fit$mcmc)}
+  draws <- do.call(rbind, fit$mcmc[chains])
+  numspecies <- fit$data$n
+  
+  if (is.null(LVvals)) {
+    if ( (is.null(fit$data$nlv)) || (fit$data$nlv == 0)){ #make dummy lvsim and and 0 loadings to draws
+      LVvals <- matrix(rnorm(2 * 1), ncol = 2, nrow = 2) #dummy lvsim vars
+      lv.coef.bugs <- matrix2bugsvar(matrix(0, nrow = numspecies, ncol = 2), "lv.coef")
+      lv.coef.draws <- Rfast::rep_row(lv.coef.bugs, nrow(draws))
+      colnames(lv.coef.draws) <- names(lv.coef.bugs)
+      draws <- cbind(draws, lv.coef.draws)
+    } else {
+      LVvals <- matrix(rnorm(fit$data$nlv * 1000), ncol = fit$data$nlv, nrow = 1000) #simulated lv values, should average over thousands
+    }
+  }
+  
+  Expectedspeciesnum.ModelSite.alltheta <- apply(draws, 1,
+      function(theta) expectedspeciesnum.ModelSite.theta(Xocc, Xobs,
+                                                         numspecies = numspecies,
+                                                         theta = theta,
+                                                         LVvals = LVvals))
+  # Expectedspeciesnum.ModelSite.alltheta is a matrix with two rows, each column is an entry from 'draws'.
+  # First row is expected number of species that will be detected, second row is expected number of species occupied
+  out <- mean(Expectedspeciesnum.ModelSite.alltheta)
+  return(out)
 }
--- tests/testthat/test-expectedbiodiversity.R
+++ tests/testthat/test-expectedbiodiversity.R
@@ -7,10 +7,14 @@ test_that("Number of detected species expected on artifical fitted model with no
                                ObsFmla = "~ 1",
                                OccFmla = "~ 1")
   theta <- get_theta(artfit, type = 1)
+  lvsim <- matrix(rnorm(2 * 1), ncol = 2, nrow = 2) #dummy lvsim vars
+  lv.coef.bugs <- matrix2bugsvar(matrix(0, nrow = artfit$data$n, ncol = 2), "lv.coef")
+  theta <- c(theta, lv.coef.bugs)
   Enumspecdet <- expectedspeciesnum.ModelSite.theta(artfit$data$Xocc[1, , drop = FALSE],
                                                     artfit$data$Xobs[artfit$data$ModelSite == 1, , drop = FALSE],
                                                     numspecies = 4,
-                                                    theta = get_theta(artfit, type = 1))
+                                                    theta = theta,
+                                                    LVvals = lvsim)
   
   # check that many other sites have the same expected number of species
   Enumspecdet_l <- lapply(1:100,
@@ -18,7 +22,8 @@ test_that("Number of detected species expected on artifical fitted model with no
            expectedspeciesnum.ModelSite.theta(artfit$data$Xocc[idx, , drop = FALSE],
                                               artfit$data$Xobs[artfit$data$ModelSite == idx, , drop = FALSE],
                                               numspecies = 4,
-                                              theta = theta)
+                                              theta = theta,
+                                              LVvals = lvsim)
          })
   expect_equal(unlist(Enumspecdet_l) - Enumspecdet, rep(0, length(Enumspecdet_l)))
   
@@ -31,27 +36,28 @@ test_that("Number of detected species expected on artifical fitted model with no
   NumSpecies <- cbind(SpDetected[, 1] , numspecies = rowSums(SpDetected[, -1]))
   cmeannumspecies <- dplyr::cummean(NumSpecies[, "numspecies"])
   
+  # check within standard error
+  se <- sd(NumSpecies[, "numspecies"]) / sqrt(nrow(NumSpecies))
+  expect_equal(cmeannumspecies[length(cmeannumspecies)], Enumspecdet, tol = 3*se)
+  
   # check that is getting closer with increasing data
   expect_lt(abs(cmeannumspecies[length(cmeannumspecies)] - Enumspecdet),
             abs( mean(cmeannumspecies[floor(length(cmeannumspecies) / 4) + 1:10]) - Enumspecdet))
   
-  # check within standard error
-  se <- sd(NumSpecies[, "numspecies"]) / sqrt(nrow(NumSpecies))
-  expect_equal(cmeannumspecies[length(cmeannumspecies)], Enumspecdet, tol = 3*se)
 })
 
 
 test_that("Number of detected species expected on artifical fitted model with covariates and no LV", {
-  nsites <- 2000
+  nsites <- 5000
   artfit <- artificial_runjags(nspecies = 5, nsites = nsites, nvisitspersite = 2, nlv = 0)
   theta <- get_theta(artfit, type = 1)
   # check that many other sites have the same expected number of species
   Enumspecdet_l <- lapply(1:nsites,
                           function(idx){
-                            expectedspeciesnum.ModelSite.theta(artfit$data$Xocc[idx, , drop = FALSE],
-                                                               artfit$data$Xobs[artfit$data$ModelSite == idx, , drop = FALSE],
-                                                               numspecies = 2,
-                                                               theta = theta)
+                            expectedspeciesnum.ModelSite(artfit,
+                                                         artfit$data$Xocc[idx, , drop = FALSE],
+                                                         artfit$data$Xobs[artfit$data$ModelSite == idx, , drop = FALSE],
+                                                         LVvals = NULL)
                           })
 
   # treat each model site as a repeat simulation of a ModelSite (cos all the parameters are nearly identical)
@@ -64,6 +70,7 @@ test_that("Number of detected species expected on artifical fitted model with co
   
   # difference between expected and observed should be zero on average
   meandiff <- dplyr::cummean(NumSpecies[, "numspecies"] - unlist(Enumspecdet_l))
+  plot(meandiff); abline(h = 0, col = "blue")
   
   # check that is getting closer with increasing data
   expect_lt(abs(meandiff[length(meandiff)]), abs(mean(meandiff[floor(length(meandiff) / 4) + 1:10 ])))
