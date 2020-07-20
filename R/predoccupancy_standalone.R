ab8cd474b97a529468b7f447556db62ccb97b471
Kassel Hingee <kassel.hingee@gmail.com>
2020-07-20 17:42:19 +1000

    draft of standalone occupancy function finished, but need to test it with existing functions

---
+++ R/predoccupancy_standalone.R
@@ -0,0 +1,50 @@
+#' @title Standalone Occupancy Probabilities and Biodiversity Across Sites
+#' @description Operates for models without latent variables. 
+#' Given occupancy parameters, computes the probability of each species being detected at each site and at *any* site.
+#' @details From Xocc a matrix of the model's cannonical covariates will be computed and then centred and scaled according to [XoccProcess].
+#' The chance of occupancy of each species (the model without latent variables assumes species are independent) is then computed using the supplied parameter matrix u.b
+#' @examples
+#' indata <- readRDS("./private/data/clean/7_2_10_input_data.rds")
+#' Xocc <- indata$holdoutdata$Xocc[1:5, ]
+#' fit <- readRDS("./tmpdata/7_2_13_rsonly_woody500m.rds")
+#' XoccProcess <- fit$XoccProcess
+#' u.b <- bugsvar2matrix(get_theta(fit, type = "median"), "u.b", rowidx = 1:fit$data$n, colidx = 1:ncol(fit$data$Xocc))
+#' rownames(u.b) <- fit$species
+#' 
+#' poccupancy <- poccupancy_standalone_nolv(Xocc, XoccProcess, u.b)
+#' pocc_any <- poccupancy_indsites_nolv(poccupancy)
+#' Erichness <- sum(pocc_any) #number of species
+#' Vrichness <- sum(pocc_any * (1 - pocc_any))
+#' multisiterichness_nolv(Xocc, XoccProcess, u.b)
+
+#' @describeIn predict_standalone Predicts occupancy of species at each site for a model without latent variables.
+#' @return A matrix. Each row is a row of Xocc (a model site), each column is a species. Values are the probabilty a species occupies the model site.
+#' @param Xocc a matrix of covariates in same scale as inputs to the fitted model *before* scaling and centering. Each row is a ModelSite
+#' @export
+poccupancy_standalone_nolv <- function(Xocc, XoccProcess, u.b){
+  XoccStd <- model.matrix(as.formula(XoccProcess$fmla), data = as.data.frame(Xocc))
+  XoccStd <- scale(XoccStd, center = XoccProcess$center, scale = XoccProcess$scale)
+  
+  # occupancy probability
+  ModelSite.Occ.eta <- as.matrix(XoccStd) %*% t(u.b)
+  ModelSite.Occ.Pred <- 1 - pnorm(-ModelSite.Occ.eta, mean = 0, sd = 1)
+  return(ModelSite.Occ.Pred)
+}
+
+#' @param poccupancy is an output of poccupancy_standalone_nolv. Each row is a ModelSite, each column is a species.
+#' Values are the probability of a species occupying a ModelSite
+poccupancy_indsites_nolv <- function(poccupancy){
+  anyoccupancy <- 1 - Rfast::colprods(1 - poccupancy)
+  names(anyoccupancy) <- colnames(poccupancy)
+  return(anyoccupancy)
+}
+
+#' @describeIn predict_standalone For species richness across multiple independent ModelSites 
+multisiterichness_nolv <- function(Xocc, XoccProcess, u.b){
+  poccupancy <- poccupancy_standalone_nolv(Xocc, XoccProcess, u.b)
+  pocc_any <- poccupancy_indsites_nolv(poccupancy)
+  Erichness <- sum(pocc_any) #number of species
+  Vrichness <- sum(pocc_any * (1 - pocc_any))
+  return(c(Erichness = Erichness,
+           Vrichness = Vrichness))
+}
