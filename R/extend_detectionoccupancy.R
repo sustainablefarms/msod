ae04e61f3715aaf6fb0fab776d0d7528c1709f5e
Kassel Hingee <kassel.hingee@gmail.com>
2020-08-30 21:28:03 +1000

    extend.fit draft written

---
+++ R/extend_detectionoccupancy.R
@@ -0,0 +1,36 @@
+#' @title Extend MCMC chains of fitted model
+#' @param fit Model fitted by run.detectionoccupancy
+#' @param ... arguments passed to extent.jags
+#' @param filename File to save extended chains (with modelling information) to.
+#' @export
+extend.fit <- function(fit, ..., filename = NULL){
+  if (!is.null(filename)){checkwritable(filename)} # check that file can be written before continuing
+  
+  # save fit$data in list form
+  data.list <- fit$data
+  
+  fit$data <- dump.format(fit$data)
+  fit2 <- extend.jags(fit, ...)
+  
+  # add summary of parameter distributions
+  if (runjagsargs$sample >= 100) {
+    fit2 <- add.summary(fit2)
+    fit2$crosscorr <- "Crosscorrelation removed to conserve disk size. See ?add.summary to compute it."
+  }
+  
+  # pass meta information
+  colnames(fit2$data$y) <- fit$species
+  colnames(fit2$data$Xocc) <- colnames(data.list$Xocc)
+  rownames(fit2$data$Xocc) <- 1:nrow(data.list$Xocc)
+  colnames(fit2$data$Xobs) <- colnames(data.list$Xobs)
+  rownames(fit2$data$Xobs) <- fit$ModelSite
+  
+  
+  # attach data preparation methods
+  fit2$XoccProcess <- fit$XoccProcess
+  fit2$XobsProcess <- fit$XobsProcess
+  fit2$ModelSite <- fit$ModelSite
+  fit2$species <- fit$species
+  if (!is.null(filename)){try(saveRDS(fit, filename)) }
+  return(invisible(fit2))
+}
