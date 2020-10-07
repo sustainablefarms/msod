848da6041f69c18c6588864d3d91978b079adbee
Kassel Hingee <kassel.hingee@gmail.com>
2020-10-08 10:40:23 +1100

    alterations to help

--- R/poccupy.R
+++ R/poccupy.R
@@ -1,11 +1,11 @@
-
+#' @title Probability of each species occupancy marginal across other species, from the full posterior distribution.
 #' @param LVvals If a matrix then LVvals are considered simulated and for each possible set of parameters,
 #' the probabilities are marginalised of the (prior) LV distribution. 
 #' If `LVvals` is an array with 3 dimensions, then the third dimension corresponds to the posterior distribution draw (same as u.b_arr).
 #' In this situation the array must have only one row: the row corresponding the model site.
 #' @param lv.coef_arr A 3-array of LV loadings. If `NULL` then model is assumed to not have latent variables.
 #' @param u.b_arr A 3-array of occupancy covariates. Each row is a species, each column is a covariate, and layer is a draw from the posterior.
-#' @details Compute the probability of each species individually (non-join) using the full posterior distribution.
+#' @details Compute the probability of each species individually (non-joint) using the full posterior distribution for a single ModelSite.
 poccupy.ModelSite <- function(Xocc, u.b_arr, lv.coef_arr = NULL, LVvals = NULL){
   if (is.null(lv.coef_arr) && is.null(LVvals)){
     lv.coef <- matrix(0, nrow = nrow(u.b_arr), ncol = 2)
