49f7067d5b6f49941ccc3be1637c64da7c3e9a68
Kassel Hingee <kassel.hingee@gmail.com>
2020-10-11 14:41:21 +1100

    noted that saved values generated by code at commit  816cca9

--- tests/testthat/test-calcs_saved_fit.R
+++ tests/testthat/test-calcs_saved_fit.R
@@ -22,19 +22,19 @@ test_that("Occupancy of species prediction is historically consistent", {
   expect_known_output(pocc_theta01, file = "pocc_theta01.txt", print = TRUE, update = FALSE)
 })
 
-test_that("Expected Biodiversity is Historically Consistent", {
+test_that("Expected Biodiversity is Historically Consistent", { #values generate from code at commit 072c730
   Enspecies_condLV <- predsumspecies(fit, UseFittedLV = TRUE)
-  expect_known_output(Enspecies_condLV, file = "Especrich_condLV.txt", print = TRUE, update = TRUE)
+  expect_known_output(Enspecies_condLV, file = "Especrich_condLV.txt", print = TRUE, update = FALSE)
   
   set.seed(1341) #for simulated LV values
   Enspecies_margLV <- predsumspecies(fit, UseFittedLV = FALSE)
-  expect_known_output(Enspecies_margLV, file = "Especrich_margLV.txt", print = TRUE, update = TRUE)
+  expect_known_output(Enspecies_margLV, file = "Especrich_margLV.txt", print = TRUE, update = FALSE)
 })
 
-test_that("Expected Individual Species Detections are Historically Consistent", {
+test_that("Expected Individual Species Detections are Historically Consistent", { #values generate from code at commit 072c730
   En_condLV <- Endetect_modelsite(fit, type = 1, conditionalLV = TRUE)
-  expect_known_output(En_condLV, file = "En_condLV.txt", print = TRUE, update = TRUE)
+  expect_known_output(En_condLV, file = "En_condLV.txt", print = TRUE, update = FALSE)
   
   En_margLV <- Endetect_modelsite(fit, type = 1, conditionalLV = FALSE)
-  expect_known_output(En_margLV, file = "En_margLV.txt", print = TRUE, update = TRUE)
+  expect_known_output(En_margLV, file = "En_margLV.txt", print = TRUE, update = FALSE)
 })
