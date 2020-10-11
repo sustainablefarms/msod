fce69fd5ddc73059524dc78b8b911ae822dfb37f
Kassel Hingee <kassel.hingee@gmail.com>
2020-10-11 14:39:32 +1100

    saving expected biodiveristy and number of detections per species

--- tests/testthat/test-calcs_saved_fit.R
+++ tests/testthat/test-calcs_saved_fit.R
@@ -21,3 +21,20 @@ test_that("Occupancy of species prediction is historically consistent", {
   pocc_theta01 <- poccupy_species(fit, type = 1, conditionalLV = TRUE)
   expect_known_output(pocc_theta01, file = "pocc_theta01.txt", print = TRUE, update = FALSE)
 })
+
+test_that("Expected Biodiversity is Historically Consistent", {
+  Enspecies_condLV <- predsumspecies(fit, UseFittedLV = TRUE)
+  expect_known_output(Enspecies_condLV, file = "Especrich_condLV.txt", print = TRUE, update = TRUE)
+  
+  set.seed(1341) #for simulated LV values
+  Enspecies_margLV <- predsumspecies(fit, UseFittedLV = FALSE)
+  expect_known_output(Enspecies_margLV, file = "Especrich_margLV.txt", print = TRUE, update = TRUE)
+})
+
+test_that("Expected Individual Species Detections are Historically Consistent", {
+  En_condLV <- Endetect_modelsite(fit, type = 1, conditionalLV = TRUE)
+  expect_known_output(En_condLV, file = "En_condLV.txt", print = TRUE, update = TRUE)
+  
+  En_margLV <- Endetect_modelsite(fit, type = 1, conditionalLV = FALSE)
+  expect_known_output(En_margLV, file = "En_margLV.txt", print = TRUE, update = TRUE)
+})
