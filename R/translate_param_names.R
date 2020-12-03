ba8a6cd16940c6a4109e2973f622132a10f98e1f
Kassel Hingee <kassel.hingee@gmail.com>
2020-12-03 13:53:03 +1100

    name translation function written

---
+++ R/translate_param_names.R
@@ -0,0 +1,129 @@
+#' @title Convert old fit objects to use new parameter naming convention
+#' @description Change the variable names to new variables names
+#' @export translatefit
+#' @examples 
+#' model2lv <- readRDS("../Experiments/7_4_modelrefinement/fittedmodels/7_4_13_model_2lv_e13.rds")
+#' model2lv_new <- translatefit(model2lv)
+#' 
+# places to change in fitted model object: mcmc, summary, summaries, monitor, some in $data too, model
+# places to change in codebase: everywhere
+parnames <- matrix(c(
+  "u.b", "occ.b",
+  "lv.coef", "lv.b",
+  "LV", "lv.v",
+  "v.b", "det.b",
+  "mu.u.b",    "mu.occ.b",
+  "tau.u.b",   "tau.occ.b",
+  "sigma.u.b", "sigma.occ.b",
+  "mu.v.b",    "mu.det.b",
+  "tau.v.b",   "tau.det.b",
+  "sigma.v.b", "sigma.det.b",
+  "n", "nspecies",
+  "J", "nmodelsites",
+  "Vvisits", "nvisits",
+  "ModelSite", "ModelSite",
+  "Vocc", "noccvar",
+  "Vobs", "nobsvar",
+  "nlv", "nlv",
+  "zeroLV", "zero.lv.v",
+  "invSigmaLV", "invSigma.lv.v",
+  "lv.covspatscale", "lv.v.spatscale",
+  "lv.covtimescale", "lv.v.timescale",
+  "z", "occ.v",
+  "u", "occ.indicator"
+  ),
+  byrow = TRUE,
+  ncol = 2
+)
+colnames(parnames) <- c("old", "new")
+old2new <- tibble::deframe(data.frame(parnames))
+
+replacebeforebracket <- function(x){
+  for (i in 1:nrow(parnames)){
+    x <- gsub(paste0("^", parnames[i,"old"], "\\["), paste0(parnames[i,"new"], "\\["), x)
+  }
+  return(x)
+}
+
+replacebetweenticks <- function(x){
+  for (i in 1:nrow(parnames)){
+    x <- gsub(paste0("`", parnames[i,"old"], "`"), paste0("`", parnames[i,"new"], "`"), x)
+  }
+  return(x)
+}
+
+translatefit <- function(fit){
+  #mcmc column names
+  newcolnames <- replacebeforebracket(colnames(fit$mcmc[[1]]))
+  fit$mcmc <- lapply(fit$mcmc, function(x){
+    colnames(x) <- newcolnames
+    return(x)
+  })
+  
+  #deviance.table, pd and more don't know structure
+  
+  # end.state
+  fit$end.state <- replacebetweenticks(fit$end.state)
+  
+  #samplers
+  fit$samplers$Node <- replacebeforebracket(fit$samplers$Node)
+  
+  # burnin, sample, thin pure integers with out names
+  # model - this is impossible to do automatically. Must use another place.
+  warning("fit$model not translated")
+  
+  # data
+  fit$data <- as_list_format(fit$data)
+  newnames <- old2new[names(fit$data)]
+  newnames[is.na(newnames)] <- names(fit$data)[is.na(newnames)]
+  names(fit$data) <- newnames
+  
+  # monitor
+  fit$monitor <- old2new[fit$monitor]
+  
+  # noread.monitor
+  fit$noread.monitor
+  fit$noread.monitor <- old2new[fit$noread.monitor]
+  
+  # modules, factories, response, residual, fitted, method, method.options, timetaken, runjags.version  need none?
+  
+  # not stored: stochastic, hist, dic, 
+  
+  # summaries
+  rownames(fit$summaries) <- replacebeforebracket(rownames(fit$summaries))
+  
+  # summary
+  rownames(fit$summary$statistics) <- replacebeforebracket(rownames(fit$summary$statistics))
+  rownames(fit$summary$quantiles) <- replacebeforebracket(rownames(fit$summary$quantiles))
+  
+  # HPD
+  rownames(fit$HPD) <- replacebeforebracket(rownames(fit$HPD))
+  rownames(fit$hpd) <- replacebeforebracket(rownames(fit$hpd))
+  
+  # msce
+  names(fit$mcse$sseff) <- replacebeforebracket(rownames(fit$mcse$sseff))
+  names(fit$mcse$ssd) <- replacebeforebracket(rownames(fit$mcse$ssd))
+  names(fit$mcse$mcse) <- replacebeforebracket(rownames(fit$mcse$mcse))
+  
+  # psrf
+  if (length(fit$psrf) != 1){ rownames(fit$psrf) <- replacebeforebracket(rownames(fit$psrf))}
+  
+  # autocorr
+  colnames(fit$autocorr) <- replacebeforebracket(colnames(fit$autocorr))
+  
+  # crosscorr ignored
+  
+  # ___stochastic
+  names(fit$truestochastic) <- replacebeforebracket(names(fit$truestochastic))
+  names(fit$semistochastic) <- replacebeforebracket(names(fit$semistochastic))
+  names(fit$nonstochastic) <- replacebeforebracket(names(fit$nonstochastic))
+  
+  names(fit$discrete) <- replacebeforebracket(names(fit$discrete))
+  
+  # trace, density, histogram, ecdfplot, key, acplot, ccplot ignored
+  # ignored: "summary.available" "summary.pars"      "XoccProcess"       "XobsProcess"       "ModelSite"        
+  # [50] "species"           "quality"
+
+  return(fit)
+}
+
