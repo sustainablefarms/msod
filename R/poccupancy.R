04f767b2ad6956f7a4e68579e7818b38c4df22bc
Kassel Hingee <kassel.hingee@gmail.com>
2021-01-07 10:53:32 +1100

    correction so that lv.v are seperated from lv.b in the final poccupancy arrays, but arrays are very large

--- R/poccupancy.R
+++ R/poccupancy.R
@@ -4,15 +4,15 @@
 #'  each columns is a covariate (in same order as in fixedcovar), and each layer is a draw from the distribution of loadings.
 #' @param randomcovar An array of occupancy covariate values that samples the distribution of the covariate.
 #' Each row is a model site, each column a covariate, and each layer is an iid draw from the (usually posterior) distribution for the covariate value.
-#' @param loadrandom An array of loadings for 'randomcovar'. Each row is a species, each column a covariate, each layer a draw from the covariate distribution.
+#' @param loadrandom An array of loadings for 'randomcovar'. Each row is a species, each column a covariate, each layer a draw from the covariate distribution, and must be the same draw as loadfixed.
 #' @return An array of occupancy probability values. Each row is a modelsite, each column a species, 
-#' each layer a draw corresponding to 'loadfixed', and each layer of the 4th dimension is a draw corresponding to 'loadrandom'.
+#' each layer a draw corresponding to the loadings, and each layer of the 4th dimension is a draw corresponding to 'randomcovar'.
 
 #' @examples
 #' fixedcovar <- matrix(rnorm(10 * 5), nrow = 10, ncol = 5,
 #'                      dimnames = list(paste0("Site", LETTERS[1:10]), paste0("C", letters[1:5])))
 #' loadfixed <- array(unlist(lapply(seq(0, 12, by = 1), function(x) rnorm(7 * 5, x))), dim = c(7, 5, 12), #each layer has a larger mean
-#'                    dimnames = list(paste0("Species", LETTERS[1:7]), paste0("C", letters[1:5]), paste0("D", 1:12)))
+#'                    dimnames = list(paste0("Species", LETTERS[1:7]), paste0("C", letters[1:5]), paste0("LfD", 1:12)))
 #' poccupy_raw.jsodm(fixedcovar, loadfixed)
 #' 
 #' fitold <- readRDS("../Experiments/7_4_modelrefinement/fittedmodels/7_4_13_allhyp_vif_logwoody500m_msnm_year_Time_Wind.rds")
@@ -21,13 +21,13 @@
 #' 
 #' 
 #' randomcovar <- array(rnorm(10 * 2), dim = c(10, 2, 30),
-#'                       dimnames = list(paste0("Site", LETTERS[1:10]), paste0("C", letters[1:2]), paste0("D", 1:30)))
-#' loadrandom <- array(unlist(lapply(seq(0, 0.3, by = 0.01), function(x) rnorm(7 * 2, x, sd = 0.01))), dim = c(7, 2, 30), #each layer has a larger mean
-#'                     dimnames = list(paste0("Species", LETTERS[1:7]), paste0("C", letters[1:2]), paste0("D", 1:30)))
+#'                       dimnames = list(paste0("Site", LETTERS[1:10]), paste0("C", letters[1:2]), paste0("RcD", 1:30)))
+#' loadrandom <- array(unlist(lapply(seq(0, 0.3, by = 0.01), function(x) rnorm(7 * 2, x, sd = 0.01))), dim = c(7, 2, 12), #each layer has a larger mean
+#'                     dimnames = list(paste0("Species", LETTERS[1:7]), paste0("C", letters[1:2]), paste0("LrD", 1:12)))
 #' pocc <- poccupy_raw.jsodm_lv(fixedcovar, loadfixed, randomcovar, loadrandom)
 #' model2lv <- readRDS("../Experiments/7_4_modelrefinement/fittedmodels/7_4_13_model_2lv_e13.rds")
 #' model2lv_new <- translatefit(model2lv)
-#' pocc <- poccupy_raw.jsodm_lv(fixedcovar, loadfixed, randomcovar, loadrandom)
+#' pocc <- poccupy_indlvv.jsodm_lv(fixedcovar, loadfixed, randomcovar, loadrandom)
 #' 
 #' @export
 poccupy_raw.jsodm <- function(fixedcovar, loadfixed, randomcovar = NULL, loadrandom = NULL){
@@ -82,40 +82,40 @@ poccupy_posterior_lvv.jsodm_lv <- function(fixedcovar, loadfixed, sd_occ_indicat
   return(pocc)
 }
 
-poccupy_marg_lvv.jsodm_lv <- function(fixedcovar, loadfixed, randomcovar, loadrandom){
+poccupy_indlvv.jsodm_lv <- function(fixedcovar, loadfixed, randomcovar, loadrandom){
+  stopifnot(dim(loadfixed)[[3]] == dim(loadrandom)[[3]]) # stop if ndraw of loadrandom differs from loadfixed, this means the draws of each can't be tied together
   eta_f <- eta_fixed(fixedcovar, loadfixed)
   
   sd_occ_indicator <- apply(loadrandom, MARGIN = c(1, 3), function(x) sqrt(1- sum(x^2)))
 
-  ndraw_lvv <- dim(randomcovar)[[3]]
-  ndraw_lvb <- dim(loadrandom)[[3]]
-  ##E# stop if ndraw of loadrandom differs from loadfixed
-  eta_rand_l <- lapply(1:ndraw_lvv, function(d)
-         randomcovar[,,d] %*% t(loadrandom[,,d])) ##E# need to do the outer thing here. For each draw of lvb, do all draws of lvv.
-  eta_rand <- abind::abind(eta_rand_l, along = 3, force.array = TRUE, use.dnns = TRUE)
-  dimnames(eta_rand) <- list(dimnames(randomcovar)[[1]], dimnames(loadrandom)[[1]], dimnames(loadrandom)[[3]])
+  eta_rand <- tensor::tensor(randomcovar, loadrandom, alongA = 2, alongB = 2)
+  # eta_rand has dimensions corresponding to site x random covariate draw x species x load draw
+  eta_rand <- aperm(eta_rand, perm = c(1, 3, 4, 2)) # site x species x load draw x random covariate draw
+
+  # add eta_f, which is the same for each random covariate draw (dimension 2)
+  eta <- apply(eta_rand, MARGIN = 4, function(x) {x + eta_f})
+  # 3 array is converted to vector. I'm guessing it does this by going down row (fixed column and layer), dim(eta) corrects this in the right automatic fashion!
+  dim(eta) <- dim(eta_rand)
+  dimnames(eta) <- dimnames(eta_rand)
   
-  # need to create the 4-array eta. It needs an outer sum across the last two dimensions, but I can't see a way to do it using existing simple functions.
-  eta <- array(0, dim = c(dim(eta_f), dim(eta_rand)[[3]]),
-               dimnames = c(dimnames(eta_f), dimnames(eta_rand)[3]))
-  sitespecindex <- expand.grid(1:nrow(eta_f), 1:ncol(eta_f))
-  a <- apply(sitespecindex, MARGIN = 1,
-        function(indices){
-          vals <- outer(eta_f[indices[[1]], indices[[2]], ],
-                eta_rand[indices[[1]], indices[[2]], ], FUN = "+")
-          vals_std <- vals / sd_occ_indicator[indices[[2]], ]
-          return(list(vals_std))
-        })
-  for (indexrow in 1:nrow(sitespecindex)){
-    eta[sitespecindex[indexrow, 1], sitespecindex[indexrow, 2], , ] <-
-      a[[indexrow]][[1]]
-  }
-  # for (i in 1:nrow(eta_f)){
-  #   for (j in 1:ncol(eta_f)){
-  #     eta[i, j, , ] <- outer(eta_f[i, j, ], eta_rand[i, j, ], FUN = "+")
-  #   }
-  # }
-  stopifnot(all(eta != 0))
+  # check conversion
+  eta_t <- apply(eta_rand, MARGIN = 4, function(x) {x + eta_f * 0})
+  dim(eta_t) <- dim(eta_rand)
+  stopifnot(all(eta_t == eta_rand))
+  
+  # for each species for each load draw, eta needs to be divided by sd_occ_indicator
+  eta_s <- apply(eta, MARGIN = c(1, 4), function(x) {x / sd_occ_indicator})
+  # 3 array is converted to vector. I'm guessing it does this by going down row (fixed column and layer), dim(eta) corrects this in the right automatic fashion!
+  dim(eta_s) <- dim(eta)[c(2, 3, 1, 4)]
+  eta_s <- aperm(eta_s, perm = c(3, 1, 2, 4))
+  dimnames(eta_s) <- dimnames(eta_rand)
+  
+  # check conversion
+  eta_t <- apply(eta, MARGIN = c(1, 4), function(x) {x / (1 + 0 * sd_occ_indicator)})
+  # 3 array is converted to vector. I'm guessing it does this by going down row (fixed column and layer), dim(eta) corrects this in the right automatic fashion!
+  dim(eta_t) <- dim(eta)[c(2, 3, 1, 4)]
+  eta_t <- aperm(eta_t, perm = c(3, 1, 2, 4))
+  all(eta_t == eta)
   
   pocc <- 1 - pnorm(-eta, mean = 0, sd = 1)
   
@@ -123,18 +123,22 @@ poccupy_marg_lvv.jsodm_lv <- function(fixedcovar, loadfixed, randomcovar, loadra
     dim(fixedcovar)[[1]],
     dim(loadfixed)[[1]],
     dim(loadfixed)[[3]],
-    dim(loadrandom)[[3]]
+    dim(randomcovar)[[3]]
   )))
   return(pocc)
 }
 
-poccupy.jsodm_lv <- function(fit){
-  fixedcovar <- fit$data$Xocc
-  dimnames(fixedcovar) <- list(ModelSite = rownames(fixedcovar), Covariate = colnames(fixedcovar))
+poccupy.jsodm_lv <- function(fit, simlvnum = 100){
+  occ.v <- fit$data$Xocc
+  dimnames(occ.v) <- list(ModelSite = rownames(occ.v), Covariate = colnames(occ.v))
   
-  loadfixed <- get_occ_b(fit)
-  lv.v <- get_lv_v(fit)
+  occ.b <- get_occ_b(fit)
   lv.b <- get_lv_b(fit)
-  pocc <- poccupy_raw.jsodm_lv(fixedcovar, loadfixed, lv.v, lv.b)
+  lv.v <- array(rnorm(dim(occ.v)[[1]] * dim(lv.b)[[2]] * simlvnum), 
+                dim = c(dim(occ.v)[[1]], dim(lv.b)[[2]], simlvnum),
+                dimnames = list(ModelSite = rownames(occ.v),
+                                lv.v = paste0("lv.v", 1:dim(lv.b)[[2]]),
+                                lvdraw = 1:simlvnum))
+  pocc <- poccupy_indlvv.jsodm_lv(occ.v, occ.b, lv.v, lv.b)
   return(pocc)
 }
