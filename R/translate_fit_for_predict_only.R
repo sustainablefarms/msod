1cf808e189080720c13e5d44585a5298960ba99b
Kassel Hingee <kassel.hingee@gmail.com>
2021-01-08 16:02:41 +1100

    option cuts down number of samples too

--- R/translate_fit_for_predict_only.R
+++ R/translate_fit_for_predict_only.R
@@ -1,5 +1,6 @@
 #' @title Translate fit object into a smaller object for using in a webapp
 #' @param fit An object fitted by this package
+#' @param ndrawsample If non-null then the number of posterior draw samples to keep, must be less than the number of posterior samples in `fit`.
 #' @return An object of the same class as `fit` with nearly all data removed. Only data used for predictions remains.
 #' @examples
 #' fit <- readRDS("../Experiments/7_4_modelrefinement/fittedmodels/7_4_13_model_2lv_e13.rds")
@@ -7,9 +8,18 @@
 #' fit <- minimise_fit.jsodm_lv(fit)
 #' saveRDS(fit, file = "../farm_biodiversity_app/data/model_data.rds")
 #' @export
-minimise_fit.jsodm_lv <- function(fit){
+minimise_fit.jsodm_lv <- function(fit, ndrawsample = NULL){
   fit2 <- fit[c("mcmc", "XoccProcess", "XobsProcess", "species")]
   fit2$mcmc <- do.call(rbind, fit2$mcmc)
   fit2$mcmc <- fit2$mcmc[, grep("^(occ.b|det.b|lv.b)", colnames(fit2$mcmc))]
+  if (!is.null(ndrawsample)){
+    ndrawin <- nrow(fit2$mcmc)
+    stopifnot(ndrawsample <= ndrawin)
+    ndrawsample
+    sampleout <- seq(1, ndrawin, length.out = ndrawsample)
+    sampleout <- round(sampleout)
+    sampleout[!duplicated(sampleout)]
+    fit2$mcmc <- fit2$mcmc[sampleout, ]
+  }
   return(fit2)
 }
