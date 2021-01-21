21a2c41de5b8c948fdf85285c09a3574093340d7
Kassel Hingee <kassel.hingee@gmail.com>
2021-01-21 11:25:18 +1100

    bug fix to get_occ_b and get lv_v and get lv b

--- R/get_draws_for_parameter.R
+++ R/get_draws_for_parameter.R
@@ -6,8 +6,8 @@
 get_occ_b <- function(fit, usesummary = NULL){
   draws <- do.call(rbind, fit$mcmc)
   occ.b <- bugsvar2array(draws, "occ.b", 1:fit$data$nspecies, 1:fit$data$noccvar)
-  occ.b <- picksummary(occ.b, usesummary = usesummary)
   dimnames(occ.b) <- list(Species = fit$species, Covariate = colnames(fit$data$Xocc), Draw = 1:nrow(draws))
+  occ.b <- picksummary(occ.b, usesummary = usesummary)
   return(occ.b)
 }
 
@@ -26,8 +26,8 @@ get_det_b <- function(fit, usesummary = NULL){
 get_lv_v <- function(fit, usesummary = NULL){
   draws <- do.call(rbind, fit$mcmc)
   lv.v <- bugsvar2array(draws, "lv.v", 1:fit$data$nmodelsites, 1:fit$data$nlv)
-  lv.v <- picksummary(lv.v, usesummary = usesummary)
   dimnames(lv.v) <- list(ModelSite = rownames(fit$data$Xocc), LV = paste0("lv", 1:fit$data$nlv, ".v"), Draw = 1:nrow(draws))
+  lv.v <- picksummary(lv.v, usesummary = usesummary)
   return(lv.v)
 }
 
@@ -36,8 +36,8 @@ get_lv_v <- function(fit, usesummary = NULL){
 get_lv_b <- function(fit, usesummary = NULL){
   draws <- do.call(rbind, fit$mcmc)
   lv.b <- bugsvar2array(draws, "lv.b", 1:fit$data$nspecies, 1:fit$data$nlv)
-  lv.b <- picksummary(lv.b, usesummary = usesummary)
   dimnames(lv.b) <- list(Species = fit$species, LV = paste0("lv", 1:fit$data$nlv, ".b"), Draw = 1:nrow(draws))
+  lv.b <- picksummary(lv.b, usesummary = usesummary)
   return(lv.b)
 }
 
