b598e0656e1102d677405fe3e11af5613c6f04ba
Kassel Hingee <kassel.hingee@gmail.com>
2021-01-22 12:03:09 +1100

    bug fixes to model quality function

--- R/model_quality_stats.R
+++ R/model_quality_stats.R
@@ -4,17 +4,29 @@
 #' @param holdoutyXobs A data frame of detection information and species detections
 #' @param ModelSite Column names used to match the holdoutyXobs to sites in holdoutXocc
 #' @param cl A cluster created by parallel::makeCluster
+#' @examples 
+#' fit <- readRDS("../sflddata/private/data/testdata/cutfit_7_4_11_2LV.rds")
+#' fit <- translatefit(fit)
+#' Xocc <- unstandardise.designmatprocess(fit$XoccProcess, fit$data$Xocc)
+#' Xocc$ModelSite <- 1:nrow(Xocc)
+#' Xobs <- unstandardise.designmatprocess(fit$XobsProcess, fit$data$Xobs)
+#' Xobs$ModelSite <- fit$data$ModelSite
+#' y <- fit$data$y
+#' yXobs <- cbind(Xobs, y)
+#' cl <- parallel::makeCluster(1)
+#' quality <- modelqualstats(fit, holdoutXocc = Xocc, holdoutyXobs = yXobs, ModelSite = 'ModelSite', cl = cl)
 #' @export
-modelqualstats <- function(fit, holdoutXocc, holdoutyXobs, ModelSite, cl){
-  lpd_holdout <- lppd.newdata(fit,
-                       Xocc = holdoutXocc,
-                       yXobs = holdoutyXobs,
-                       ModelSite = ModelSite,
-                       cl = cl)
-  print("Computed: LPD of Holdout Sites")
+modelqualstats <- function(fit, holdoutXocc, holdoutyXobs, ModelSite, cl, nlvperdraw = 1000){
+  likel.mat_holdout <- apply_to_new_data(likelihood, fit, 
+                                   Xocc = holdoutXocc, 
+                                   Xobs = holdoutyXobs[, !(colnames(holdoutyXobs) %in% fit$species), drop = FALSE],
+                                   ModelSite = ModelSite,
+                                   y = holdoutyXobs[, colnames(holdoutyXobs) %in% fit$species, drop = FALSE],
+                                   funargs = list(cl = cl, numlvsims = nlvperdraw))
+  lpd_holdout <- elpd(likel.mat_holdout)
+  print("Estimated LPD of Holdout Sites")
   
-  likel.mat <- likelihoods.fit(fit, chain = NULL,
-                               cl = cl)
+  likel.mat <- likelihood(fit, cl = cl, numlvsims = nlvperdraw)
   chain_id <- lapply(1:length(fit$mcmc), function(x) rep(x, nrow(fit$mcmc[[x]])))
   chain_id <- as.integer(unlist(chain_id))
   waic <- loo::waic(log(likel.mat))
@@ -22,22 +34,31 @@ modelqualstats <- function(fit, holdoutXocc, holdoutyXobs, ModelSite, cl){
   looest <- loo::loo(log(likel.mat), r_eff = r_eff, cores = length(cl))
   print("Computed: LOO-PSIS Estimate")
   
-  prednumbers_holdout <- 
-    predsumspecies_newdata(fit,
-                           holdoutXocc,
-                           holdoutyXobs,
-                           ModelSiteVars = ModelSite,
-                           cl = cl)
-  print("Computed: Predicted Number of Species for Holdout Data")
-  
-  prednumbers_insample <- prednumbers_insample_fitLV <- prednumbers_insample_margLV <- NULL
-  if (!is.null(fit$data$nlv) && fit$data$nlv > 0){
-    prednumbers_insample_fitLV <- predsumspecies(fit, UseFittedLV = TRUE, cl = cl)
-    prednumbers_insample_margLV <- predsumspecies(fit, UseFittedLV = FALSE, cl = cl)
-  } else {
-    prednumbers_insample <- predsumspecies(fit, UseFittedLV = FALSE, cl = cl)
+
+  prednumbers_holdout <- prednumbers_insample <- prednumbers_insample_fitLV <- prednumbers_insample_margLV <- NULL
+  if ("jsodm_lv" %in% class(fit)){
+    prednumbers_holdout <- 
+      apply_to_new_data(speciesrichness, fit, 
+                        holdoutXocc, holdoutyXobs[, !(colnames(holdoutyXobs) %in% fit$species), drop = FALSE],
+                        ModelSite = "ModelSite",
+                        funargs = list(occORdetection = "detection",
+                                       usefittedlvv = FALSE,
+                                       nlvperdraw = nlvperdraw))
+    print("Computed: Predicted Number of Species for Holdout Data")
+    
+    prednumbers_insample_fitLV <- speciesrichness(fit, occORdetection = "detection", usefittedlvv = TRUE)
+    prednumbers_insample_margLV <- speciesrichness(fit, occORdetection = "detection", usefittedlvv = FALSE, nlvperdraw = nlvperdraw)
+    print("Computed: Predicted Number of Species for Insample Data")
+  } else if ("jsodm" %in% class(fit)){
+    prednumbers_holdout <- 
+      apply_to_new_data(speciesrichness,fit, 
+                        holdoutXocc, holdoutyXobs[, !(colnames(holdoutyXobs) %in% fit$species), drop = FALSE],
+                        ModelSite = "ModelSite",
+                        funargs = list(occORdetection = "detection"))
+    print("Computed: Predicted Number of Species for Holdout Data")
+    prednumbers_insample <- speciesrichness(fit, occORdetection = "detection")
+    print("Computed: Predicted Number of Species for Insample Data")
   }
-  print("Computed: Predicted Number of Species for Insample Data")
   
   quality <- list(
     holdout = list(
