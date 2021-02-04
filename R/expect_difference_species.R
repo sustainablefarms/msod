c43023d672a4402b1a5908172fab92598985b711
Kassel Hingee <kassel.hingee@gmail.com>
2021-02-04 15:22:09 +1100

    expected difference to reference site finished

--- R/expect_difference_species.R
+++ R/expect_difference_species.R
@@ -1,26 +1,36 @@
 #' difference in species richness to reference environmental variables.
+#' @examples
 #' fit <- readRDS("../sflddata/private/data/testdata/cutfit_7_4_11_2LV.rds")
-fit <- readRDS("../Experiments/7_4_modelrefinement/fittedmodels/7_4_13_model_2lv_e13.rds")
-fit <- translatefit(fit)
-Xocc <- unstandardise.designmatprocess(fit$XoccProcess, fit$data$Xocc[1, , drop = FALSE])
-refXocc <- colMeans(unstandardise.designmatprocess(fit$XoccProcess, fit$data$Xocc[, , drop = FALSE]))
-refXocc["NMdetected"] <- 1
-refXocc <- t(refXocc)
-refXocc <- as.data.frame(refXocc)
+#' fit <- readRDS("../Experiments/7_4_modelrefinement/fittedmodels/7_4_13_model_2lv_e13.rds")
+#' fit <- translatefit(fit)
+#' Xocc <- unstandardise.designmatprocess(fit$XoccProcess, fit$data$Xocc[10, , drop = FALSE])
+#' refXocc <- colMeans(unstandardise.designmatprocess(fit$XoccProcess, fit$data$Xocc[, , drop = FALSE]))
+#' refXocc["NMdetected"] <- 1
+#' refXocc <- t(refXocc)
+#' refXocc <- as.data.frame(refXocc)
+#' occspecrich_difference(fit, Xocc, refXocc)
+#' refXocc <- Xocc
+#' refXocc$woody500m <- 2
+#' refXocc$NMdetected <- 1
+#' refXocc$MaxTWarmMonth <- 370
+#' occspecrich_difference(fit, Xocc, refXocc)
 
-set.seed(3548) #so LV are the same for reference info and pocc
-pocc <- apply_to_new_data(poccupy, fit,
-                          Xocc = Xocc,
-                          funargs = list(lvvfromposterior = FALSE))
-set.seed(3548) #so LV are the same for reference info and pocc
-refpocc <- apply_to_new_data(poccupy, fit,
-                             Xocc = refXocc,
-                             funargs = list(lvvfromposterior = FALSE))
+#' @export
+occspecrich_difference <- function(fit, Xocc, refXocc, sameLVvalues = TRUE){
+  stopifnot(sameLVvalues)
+  
+  set.seed(3548) #so LV are the same for reference info and pocc
+  pocc <- apply_to_new_data(poccupy, fit,
+                            Xocc = Xocc,
+                            funargs = list(lvvfromposterior = FALSE))
+  set.seed(3548) #so LV are the same for reference info and pocc
+  refpocc <- apply_to_new_data(poccupy, fit,
+                               Xocc = refXocc,
+                               funargs = list(lvvfromposterior = FALSE))
+  
+  Ediffrv <- pocc - refpocc
+  Vdiffrv <- pocc * (1 - pocc) + refpocc * (1 - refpocc)
+  
+  return(prednumsuccess_ErvVrv(Ediffrv, Vdiffrv))
+}
 
-Ediffrv <- pocc - refpocc
-Vdiffrv <- pocc * (1 - pocc) + refpocc * (1 - refpocc)
-
-prednumsuccess_ErvVrv(Ediffrv, Vdiffrv)
-
-pocc[1, 1, 1:5]
-refpocc[1, 1, 1:5]
