e6cb5e691820c39c38d9e666b40e7134dc81e47e
Kassel Hingee <kassel.hingee@gmail.com>
2021-12-10 08:40:54 +1100

    function for computing expected number of detections per species

---
+++ R/Edetperspecies.R
@@ -0,0 +1,51 @@
+#' @title Expected number of detections per species per site
+#' @param fit A fitted jsodm
+#' @param desiredspecies List of species names to include in the calculations
+#' @return An array of expected number of detections per modelsite x species x draw.
+#' @description For each species individually calculates the expected number of detections at each ModelSite.
+#' As this is the expected value, and each species treated individually, the same method can be applied to jsodm_lv models.
+#' @examples 
+#' fit <- readRDS("../sflddata/private/data/testdata/cutfit_7_4_11_2LV.rds")
+#' fit <- translatefit(fit)
+#' Endet <- Edetperspecies.jsodm(fit)
+#' @export
+Edetperspecies.jsodm <- function(fit, 
+                                  desiredspecies = fit$species){
+  stopifnot(all(desiredspecies %in% fit$species))
+  occ.v <- fit$data$Xocc
+  occ.b <- get_occ_b(fit)[desiredspecies, , , drop = FALSE]
+  pocc <- poccupy_raw.jsodm(occ.v, occ.b)
+  det.b <- get_det_b(fit)[desiredspecies, , , drop = FALSE]
+  det.v <- fit$data$Xobs
+    
+  pdet_occ <- pdet_occ_raw.jsodm(det.v, det.b)
+  Endet <- detperspecies_probarr(pocc, pdet_occ, fit$data$ModelSite)
+  return(Endet)
+}
+ 
+# pocc = 3 array of probability of occupancy. Within each draw occupancy of species considered independent.
+# pdet_occ = 3 array of probability of detection, condition on occupancy. Within each draw species detection considered independent given occupied.
+# returns an array of expected detection number. Dimensions of modelsite, species, draw
+detperspecies_probarr <- function(pocc, pdet_occ, ModelSite){
+  ModSiteVisits <- plyr::split_indices(ModelSite)
+  names(ModSiteVisits) <- lapply(ModSiteVisits, function(x) ModelSite[x[[1]]]) %>% unlist()
+  
+  # per species expected number of detections in a site when the site is occupied. 
+  # Depends on the number of visits
+  Endet_occ <- vapply(ModSiteVisits,
+                             function(visits){
+                               Endet <- apply(pdet_occ[visits, , , drop = FALSE], MARGIN = c(2, 3), sum)
+                               return(Endet)
+                             },
+                             FUN.VALUE = matrix(1.1, nrow = ncol(pdet_occ), ncol = dim(pdet_occ)[[3]]))
+  if (is.null(dim(Endet_occ))) {
+    dim(Endet_occ) <- dim(pocc)[c(2, 3, 1)]
+    dimnames(Endet_occ) <- dimnames(pocc)[c(2, 3, 1)]
+  }
+  #Endet_occ array of species x draw x ModelSite
+  Endet_occ <- aperm(Endet_occ, perm = c(3, 1, 2)) #now Model Site x Species x Draw
+  
+  ## probability of no detections for all visits to a site, marginal on occupancy
+  Endet <- Endet_occ * pocc + 0
+  return(Endet)
+}
